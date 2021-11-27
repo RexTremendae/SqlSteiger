0e6c4d70336b475e87cb7c92709572e8360ec42d
Per Westerlund <per@fastmail.com>
2021-11-27 16:25:16 +0100

    Moved table, relations and data output logic to separate class

---
+++ Informator.cs
@@ -0,0 +1,97 @@
+using static System.Console;
+
+namespace SqlDataExtractor
+{
+    using ForeignKeyMap = Dictionary<(string table, string column), (string table, string column)>;
+
+    public static class Informator
+    {
+        private static ConsoleColor FrameColor = ConsoleColor.White;
+
+        public static void PrintTables(IEnumerable<DatabaseTableMetadata> tables)
+        {
+            ForegroundColor = FrameColor;
+            WriteLine("----------");
+            WriteLine("- Tables -");
+            WriteLine("----------");
+            ResetColor();
+            foreach (var tbl in tables)
+            {
+                var columnListing = string.Join(", ",
+                    tbl.Columns.Select(c => $"{c.Name} [{c.SqlDataType}::{c.CSharpDataType}]"));
+                WriteLine($"{tbl.Name} ({columnListing})");
+            }
+            WriteLine();
+        }
+
+        public static void PrintRelations(ForeignKeyMap foreignKeys)
+        {
+            ForegroundColor = FrameColor;
+            WriteLine("-------------");
+            WriteLine("- Relations -");
+            WriteLine("-------------");
+            ResetColor();
+            foreach (var (from, to) in foreignKeys)
+            {
+                WriteLine($"{from.table}.{from.column} => {to.table}.{to.column}");
+            }
+            WriteLine();
+        }
+
+        public static async Task PrintDataAsync(IDbConnection connection, IEnumerable<DatabaseTableMetadata> tables)
+        {
+            foreach (var tbl in tables)
+            {
+                var data = new List<string[]>(new[] { tbl.Columns.Select(c => c.Name).ToArray() });
+                var maxSize = tbl.Columns.Select(c => c.Name.Length).ToArray();
+
+                var cmd = connection.CreateCommand(tbl.CreateSelectQuery());
+                await using var reader = await cmd.ExecuteReaderAsync();
+
+                while (await reader.ReadAsync())
+                {
+                    var dataRow = new List<string>();
+                    for (int i = 0; i < tbl.Columns.Length; i++)
+                    {
+                        var col = tbl.Columns[i];
+                        dataRow.Add(reader.GetValue(col.Name, col.CSharpDataType)?.ToString() ?? "<NULL>");
+                        maxSize[i] = Math.Max(maxSize[i], dataRow[i].Length);
+                    }
+                    data.Add(dataRow.ToArray());
+                }
+
+                ForegroundColor = ConsoleColor.White;
+                WriteLine($"- Data: {tbl.Name} -");
+                WriteLine("".PadLeft(maxSize.Sum() + tbl.Columns.Length*3 + 1, '-'));
+
+                var firstRow = true;
+                foreach (var row in data)
+                {
+                    ForegroundColor = ConsoleColor.White;
+                    Write("|");
+                    ResetColor();
+                    for (int i = 0; i < maxSize.Length; i ++)
+                    {
+                        Write($" {row[i].PadRight(maxSize[i])} ");
+                        ForegroundColor = ConsoleColor.White;
+                        Write("|");
+                        ResetColor();
+                    }
+                    WriteLine();
+
+                    if (firstRow)
+                    {
+                        ForegroundColor = ConsoleColor.White;
+                        WriteLine("".PadLeft(maxSize.Sum() + row.Length*3 + 1, '-'));
+                        firstRow = false;
+                        ResetColor();
+                    }
+                }
+                ForegroundColor = ConsoleColor.White;
+                WriteLine("".PadLeft(maxSize.Sum() + tbl.Columns.Length*3 + 1, '-'));
+                ResetColor();
+                WriteLine();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Data.SqlClient;
 using SqlDataExtractor;
 using SqlDataExtractor.SqlDatabase;
-using static System.Console;
 
 var connectionBuilder = new SqlConnectionStringBuilder();
 
@@ -19,69 +18,9 @@ await using IDbConnection connection = new SqlDbConnection(connectionBuilder.ToS
 await connection.OpenAsync();
 var databaseStructureExtractor = new DatabaseStructureExtractor(connection);
 
-var tables = await databaseStructureExtractor.ExtractTableMap();
+var tables = (await databaseStructureExtractor.ExtractTableMap()).Select(t => t.Value);
 var foreignKeys = await databaseStructureExtractor.ExtractForeignKeyMap();
 
-WriteLine("----------");
-WriteLine("- Tables -");
-WriteLine("----------");
-foreach (var (name, tbl) in tables)
-{
-    var columnListing = string.Join(", ",
-        tbl.Columns.Select(c => $"{c.Name} [{c.SqlDataType}::{c.CSharpDataType}]"));
-    WriteLine($"{tbl.Name} ({columnListing})");
-}
-WriteLine();
-
-WriteLine("-------------");
-WriteLine("- Relations -");
-WriteLine("-------------");
-foreach (var (from, to) in foreignKeys)
-{
-    WriteLine($"{from} => {to}");
-}
-WriteLine();
-
-
-foreach (var (_, tbl) in tables)
-{
-    var data = new List<string[]>(new[] { tbl.Columns.Select(c => c.Name).ToArray() });
-    var maxSize = tbl.Columns.Select(c => c.Name.Length).ToArray();
-
-    var cmd = connection.CreateCommand(tbl.CreateSelectQuery());
-    await using var reader = await cmd.ExecuteReaderAsync();
-
-    while (await reader.ReadAsync())
-    {
-        var dataRow = new List<string>();
-        for (int i = 0; i < tbl.Columns.Length; i++)
-        {
-            var col = tbl.Columns[i];
-            dataRow.Add(reader.GetValue(col.Name, col.CSharpDataType)?.ToString() ?? "<NULL>");
-            maxSize[i] = Math.Max(maxSize[i], dataRow[i].Length);
-        }
-        data.Add(dataRow.ToArray());
-    }
-
-    WriteLine($"- Data: {tbl.Name} -");
-    WriteLine("".PadLeft(maxSize.Sum() + tbl.Columns.Length*3 + 1, '-'));
-
-    var firstRow = true;
-    foreach (var row in data)
-    {
-        Write("|");
-        for (int i = 0; i < maxSize.Length; i ++)
-        {
-            Write($" {row[i].PadRight(maxSize[i])} |");
-        }
-        WriteLine();
-
-        if (firstRow)
-        {
-            WriteLine("".PadLeft(maxSize.Sum() + row.Length*3 + 1, '-'));
-            firstRow = false;
-        }
-    }
-    WriteLine("".PadLeft(maxSize.Sum() + tbl.Columns.Length*3 + 1, '-'));
-    WriteLine();
-}
+Informator.PrintTables(tables);
+Informator.PrintRelations(foreignKeys);
+await Informator.PrintDataAsync(connection, tables);
