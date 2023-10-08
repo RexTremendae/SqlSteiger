57136ddbf563864bfc4fc42b4f1ef4349d180a7e
Per Westerlund <per@fastmail.se>
2023-10-08 21:03:05 +0200

    SA1008: Opening parenthesis should not be preceded by a space

--- src/SqlDataExtractor/Records.cs
+++ src/SqlDataExtractor/Records.cs
@@ -2,7 +2,30 @@ namespace SqlDX;
 
 using System.Data;
 
-public readonly record struct DatabaseColumnMetadata (string Name, SqlDbType SqlDataType, Type CSharpDataType, bool IsNullable, bool IsIdentity, bool IsPrimaryKeyPart);
-public readonly record struct DatabaseTableMetadata (string Schema, string Name, DatabaseColumnMetadata[] Columns);
-public readonly record struct InsertQueryBuildingBlocks (DatabaseTableMetadata TableMetadata, IEnumerable<Dictionary<string, object?>> DataRows);
-public readonly record struct InsertQueryConfiguration (int MaxRowBatchSize);
+public readonly record struct DatabaseColumnMetadata
+(
+    string Name,
+    SqlDbType SqlDataType,
+    Type CSharpDataType,
+    bool IsNullable,
+    bool IsIdentity,
+    bool IsPrimaryKeyPart
+);
+
+public readonly record struct DatabaseTableMetadata
+(
+    string Schema,
+    string Name,
+    DatabaseColumnMetadata[] Columns
+);
+
+public readonly record struct InsertQueryBuildingBlocks
+(
+    DatabaseTableMetadata TableMetadata,
+    IEnumerable<Dictionary<string, object?>> DataRows
+);
+
+public readonly record struct InsertQueryConfiguration
+(
+    int MaxRowBatchSize
+);
